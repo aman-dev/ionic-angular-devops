98a2a688ab29f28e6cd60f15000061f37b2c9f2e
aeciomiguel <aeciomiguel@gmail.com>
2017-04-11 22:43:14 +0200

    Renamed ionic-devops-fusion

---
+++ ionic-devops-fusion/server/config.ts
@@ -0,0 +1,23 @@
+export const devVariables:any = {
+  environmentName: 'Development Environment',
+  serverEnvName: 'dev',
+  // Back-end
+  dbHost: 'mongodb://localhost:27017',
+  dbName: 'test'
+};
+
+
+declare const process: any; // Typescript compiler will complain without this
+
+export function environmentConfig():any {
+  let env = devVariables;
+  // if(process.env.NODE_ENV === 'pre-prod'){env = prodVariables}
+  return env;
+}
+
+export const SECRET_TOKEN_KEY: string = 'this is a bad secret sentence';
+export const DB_NAME: string = environmentConfig().dbName;
+export const DB_HOST: string = environmentConfig().dbHost;
+export const BCRYPT_ROUND: number = 10;
+export const PASSWORD_MIN_LENGHT: number = 6;
+export const JWT_EXPIRE: number = 86400000;
